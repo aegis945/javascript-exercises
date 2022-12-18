5fa22d26237a806acf0de13e86d4410a55ecad86
aegis945 <artiom.lekincev@gmail.com>
2022-12-18 23:00:11 +0200

    completed 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+    let string = [];
+    string += input;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
