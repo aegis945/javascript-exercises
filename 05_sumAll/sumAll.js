0419c725bda80d9fe56a5694517d819eeb1a1e05
aegis945 <artiom.lekincev@gmail.com>
2022-12-20 00:53:34 +0200

    completed 2 more exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function (firstInt, secondInt) {
+    let sum = 0;
+    if ((firstInt < 0) || (secondInt < 0) || (!Number.isInteger(firstInt)) || (!Number.isInteger(secondInt))) {
+        return "ERROR";
+    }
+    if (firstInt < secondInt) {
+        for (let i = firstInt; i <= secondInt; i++) {
+            sum += i;
+        }
+    }
+    if (firstInt > secondInt) {
+        for (let i = secondInt; i <= firstInt; i++) {
+            sum += i;
+        }
+    };
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
