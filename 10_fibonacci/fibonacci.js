1cd61680081c8885a06c06d8bc1235e94b008442
aegis945 <artiom.lekincev@gmail.com>
2022-12-21 01:49:08 +0200

    fibonacci solved using Binet's formula

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+/* Using Binet's formula to find the nth term of Fibonacci sequence */
+const fibonacci = function (num) {
+    if (num > 0) {
+        var firstPhi = Math.pow(((1 + Math.sqrt(5)) / 2), num);
+        var secondPhi = Math.pow(((1 - Math.sqrt(5)) / 2), num);
+        return fibonacciNumber = Math.floor((firstPhi - secondPhi) / Math.sqrt(5));
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
